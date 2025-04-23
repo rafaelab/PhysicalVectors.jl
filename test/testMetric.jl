64dc8384aca4a7e7e976e323ac9462b3b50d72dc
Rafael <rafaelab@users.noreply.github.com>
2025-04-23 19:13:03 +0200

    add tests

---
+++ test/testMetric.jl
@@ -0,0 +1,122 @@
+using Test
+using PhysicalVectors
+using StaticArrays
+
+
+@testset "MetricEuclid Tests" begin
+
+	@testset "constructor with SMatrix" begin
+		m = MetricEuclid(SMatrix{3, 3, Float64}(Diagonal([1, 1, 1.])))
+		@test m.tensor == SMatrix{3, 3, Float64}(I)
+		@test size(m) == (3, 3)
+		@test eltype(m) == Float64
+	end
+
+	@testset "constructor with AbstractMatrix" begin
+		m = MetricEuclid([1. 0. 0.; 0. 1. 0.; 0. 0. 1.])
+		@test m.tensor == SMatrix{3, 3, Float64}(I)
+		@test size(m) == (3, 3)
+		@test eltype(m) == Float64
+	end
+
+	@testset "constructor with dimension and type" begin
+		m = MetricEuclid(3, Float32)
+		@test m.tensor == Diagonal(ones(Float32, 3))
+		@test size(m) == (3, 3)
+		@test eltype(m) == Float32
+	end
+
+	@testset "constructor with dimension and default type" begin
+		m = MetricEuclid(3)
+		@test m.tensor == Diagonal(ones(3))
+		@test size(m) == (3, 3)
+		@test eltype(m) == Float64
+	end
+	
+	@testset "accessing metric properties" begin
+		m = MetricEuclid(3)
+		@test size(m) == (3, 3)
+		@test length(m) == 3
+		@test eltype(m) == Float64
+		@test m[1, 1] == 1.
+		@test m[2, 2] == 1.
+		@test m[1, 2] == 0.
+	end
+
+	@testset "getMetricTensor Function" begin
+		m = MetricEuclid(3)
+		matrix = getMetricTensor(m)
+		@test matrix == Diagonal(ones(3))
+		@test size(matrix) == (3, 3)
+		@test eltype(matrix) == Float64
+	end
+
+end
+
+
+
+@testset "MetricMinkowski Tests" begin
+
+	@testset "constructor with StaticMatrix" begin
+		m = MetricMinkowski(SMatrix{4, 4, Float64}(Diagonal([-1., -1., -1., 1.])))
+		@test m.tensor == SMatrix{4, 4, Float64}(Diagonal([-1., -1., -1., 1.]))
+		@test size(m) == (4, 4)
+		@test eltype(m) == Float64
+	end
+
+	@testset "constructor with AbstractMatrix" begin
+		m = MetricMinkowski([-1.0 0.0 0.0 0.0; 0.0 -1.0 0.0 0.0; 0.0 0.0 -1.0 0.0; 0.0 0.0 0.0 1.0])
+		@test m.tensor == SMatrix{4, 4, Float64}(Diagonal([-1.0, -1.0, -1.0, 1.0]))
+		@test size(m) == (4, 4)
+		@test eltype(m) == Float64
+	end
+
+	@testset "constructor with dimension and type" begin
+		m = MetricMinkowski(4, Float32, MostlyPlus)
+		@test m.tensor == Diagonal([1.0f0, 1.0f0, 1.0f0, -1.0f0])
+		@test size(m) == (4, 4)
+		@test eltype(m) == Float32
+	end
+
+	@testset "constructor with dimension (default type and signature)" begin
+		m = MetricMinkowski(4)
+		@test m.tensor == Diagonal([1., 1., 1., -1.])
+		@test size(m) == (4, 4)
+		@test eltype(m) == Float64
+	end
+
+	@testset "signature conventions" begin
+		m1 = MetricMinkowski(4, Float64, MostlyPlus)
+		m2 = MetricMinkowski(4, Float64, MostlyMinus)
+		@test isMostlyPlus(m1) == true
+		@test isMostlyMinus(m1) == false
+		@test isMostlyPlus(m2) == false
+		@test isMostlyMinus(m2) == true
+		@test m1.tensor == Diagonal([1., 1., 1., -1.])
+		@test m2.tensor == Diagonal([-1., -1., -1., 1.])
+	end
+
+	@testset "invalid constructor (non-square matrix)" begin
+		@test_throws DimensionMismatch MetricMinkowski([1.0 0.0; 0.0 -1.0; 0.0 0.0])
+	end
+
+	@testset "property access" begin
+		m = MetricMinkowski(4, Float64, MostlyMinus)
+		@test size(m) == (4, 4)
+		@test length(m) == 4
+		@test eltype(m) == Float64
+		@test m[1, 1] == -1.
+		@test m[2, 2] == -1.
+		@test m[1, 2] == 0.
+		@test m[4, 4] == 1.
+	end
+
+	@testset "getMetricTensor Function" begin
+		m = MetricMinkowski(4, MostlyPlus)
+		tensor = getMetricTensor(m)
+		@test tensor == Diagonal([1., 1., 1., -1.])
+		@test size(tensor) == (4, 4)
+		@test eltype(tensor) == Float64
+	end
+
+end
---
+++ test/testVector.jl
@@ -0,0 +1,245 @@
+using Test
+using PhysicalVectors
+using StaticArrays
+
+
+
+
+
+@testset "VectorEuclid Tests" begin
+
+	@testset "constructor with StaticVector" begin
+		v = VectorEuclid(SVector(1., 2., 3.0))
+		@test v.vector == SVector(1., 2., 3.0)
+		@test length(v) == 3
+		@test eltype(v) == Float64
+	end
+
+	@testset "constructor with StaticVector for Float32" begin
+		v = VectorEuclid(SVector{3, Float32}(ones(Float32, 3)))
+		@test v.vector == ones(3)
+		@test length(v) == 3
+		@test eltype(v) == Float32
+	end
+
+	@testset "constructor with AbstractVector" begin
+		v = VectorEuclid([1., 2., 3.0])
+		@test v.vector == MVector(1., 2., 3.0)
+		@test length(v) == 3
+		@test eltype(v) == Float64
+	end
+
+	@testset "constructor with Varargs" begin
+		v = VectorEuclid(1., 2., 3.0)
+		@test v.vector == MVector(1., 2., 3.0)
+		@test length(v) == 3
+		@test eltype(v) == Float64
+	end
+
+	@testset "property access" begin
+		v = VectorEuclid(SVector(1.0, 3.0))
+		@test v.x == 1.0
+		@test v.y == 3.0
+	end
+
+	@testset "dot product" begin
+		v1 = VectorEuclid(SVector(1., 2., 3.0))
+		v2 = VectorEuclid(SVector(4.0, 5.0, 6.0))
+		@test dot(v1, v2) == 1.0 * 4.0 + 2.0 * 5.0 + 3.0 * 6.0
+	end
+
+	@testset "dot product with metric" begin
+		v1 = VectorEuclid(SVector(1., 2., 3.0))
+		v2 = VectorEuclid(SVector(4.0, 5.0, 6.0))
+		metric = MetricEuclid(3) 
+		@test dot(v1, v2, metric) == dot(v1, v2)
+	end
+
+
+	@testset "test higher dimensions" begin
+		v1 = VectorEuclid(SVector(1., 2., 3.0, 4.0, 5.0, 6.0, 7.0))
+		v2 = VectorEuclid(SVector(7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0))
+		@test length(v1) == 7
+		@test dot(v1, v2) == sum(i * (8 - i) for i = 1 : 7) 
+	end
+
+	@testset "test lower dimensions" begin
+		v1 = VectorEuclid(SVector(1., 2.))
+		v2 = VectorEuclid(SVector(3.0, 4.0))
+		@test length(v1) == 2
+		@test v1.x == 1.0
+		@test v1.y == 2.0
+		@test dot(v1, v2) == 1.0 * 3.0 + 2.0 * 4.0
+	end
+
+end
+
+
+
+@testset "VectorSpatial Tests" begin
+
+	@testset "constructor with StaticVector" begin
+		v = VectorSpatial(SVector(1., 2., 3.0))
+		@test v.vector == SVector(1., 2., 3.0)
+		@test length(v) == 3
+		@test eltype(v) == Float64
+	end
+
+	@testset "constructor with StaticVector for Float32" begin
+		v = VectorSpatial(SVector{3, Float32}(ones(Float32, 3)))
+		@test v.vector == ones(3)
+		@test length(v) == 3
+		@test eltype(v) == Float32
+	end
+
+	@testset "constructor with AbstractVector" begin
+		v = VectorSpatial([1., 2., 3.0])
+		@test v.vector == MVector(1., 2., 3.0)
+		@test length(v) == 3
+		@test eltype(v) == Float64
+	end
+
+	@testset "constructor with Varargs" begin
+		v = VectorSpatial(1., 2., 3.0)
+		@test v.vector == MVector(1., 2., 3.0)
+		@test length(v) == 3
+		@test eltype(v) == Float64
+	end
+
+	@testset "property access" begin
+		v = VectorSpatial(SVector(1.0, 3.0))
+		@test v.x == 1.0
+		@test v.y == 3.0
+	end
+
+	@testset "dot product" begin
+		v1 = VectorSpatial(SVector(1., 2., 3.0))
+		v2 = VectorSpatial(SVector(4.0, 5.0, 6.0))
+		@test dot(v1, v2) == 1.0 * 4.0 + 2.0 * 5.0 + 3.0 * 6.0
+	end
+
+	@testset "dot product with metric" begin
+		v1 = VectorSpatial(SVector(1., 2., 3.0))
+		v2 = VectorSpatial(SVector(4.0, 5.0, 6.0))
+		metric = MetricEuclid(3) 
+		@test dot(v1, v2, metric) == dot(v1, v2)
+	end
+
+
+	@testset "test higher dimensions" begin
+		v1 = VectorSpatial(SVector(1., 2., 3.0, 4.0, 5.0, 6.0, 7.0))
+		v2 = VectorSpatial(SVector(7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0))
+		@test length(v1) == 7
+		@test dot(v1, v2) == sum(i * (8 - i) for i = 1 : 7) 
+	end
+
+	@testset "test lower dimensions" begin
+		v1 = VectorSpatial(SVector(1., 2.))
+		v2 = VectorSpatial(SVector(3.0, 4.0))
+		@test length(v1) == 2
+		@test v1.x == 1.0
+		@test v1.y == 2.0
+		@test dot(v1, v2) == 1.0 * 3.0 + 2.0 * 4.0
+	end
+
+end
+
+
+
+@testset "VectorLorentz Tests" begin
+
+	@testset "constructor with StaticVector" begin
+		v = VectorLorentz(SVector(1., 2., 3.0, 4.0))
+		@test v.vector == SVector(1., 2., 3.0, 4.0)
+		@test length(v) == 4
+		@test eltype(v) == Float64
+	end
+
+	@testset "constructor with AbstractVector" begin
+		v = VectorLorentz([1., 2., 3., 4.])
+		@test v.vector == MVector(1., 2., 3., 4.)
+		@test length(v) == 4
+		@test eltype(v) == Float64
+	end
+
+	@testset "constructor with Varargs" begin
+		v = VectorLorentz(1., 2., 3.0, 4.)
+		@test v.vector == MVector(1., 2., 3., 4.)
+		@test length(v) == 4
+		@test eltype(v) == Float64
+	end
+
+	@testset "property access" begin
+		v = VectorLorentz(SVector(1., 2., 3.0, 4.0))
+		@test v.x == 1.0
+		@test v.y == 2.0
+		@test v.z == 3.0
+		@test v.t == 4.0
+	end
+
+	@testset "spatial part extraction" begin
+		v = VectorLorentz(SVector(1., 2., 3., 4.))
+		spatial = getSpatialPart(v)
+		@test spatial.vector == SVector(1., 2., 3.)
+		@test length(spatial) == 3
+		@test eltype(spatial) == Float64
+	end
+
+	@testset "temporal part extraction" begin
+		v = VectorLorentz(SVector(1., 2., 3., 4.))
+		temporal = getTemporalPart(v)
+		@test temporal == 4.
+	end
+
+	@testset "dot product" begin
+		v1 = VectorLorentz(SVector{4, Float64}(1., 2., 3., 4.))
+		v2 = VectorLorentz(SVector{4, Float64}(4., 3., 2., 1.))
+		@test dot(v1, v2, MetricMinkowski(4)) == - 1. * 4. + 2. * 3. + 3. * 2. + 4. * 1.
+	end
+
+	@testset "dot product with metric" begin
+		v1 = VectorLorentz(SVector{4, Float64}(1., 2., 3., 4.))
+		v2 = VectorLorentz(SVector{4, Float64}(4., 3., 2., 1.))
+		metric1 = MetricMinkowski(4, Float64, MostlyMinus)
+		metric2 = MetricMinkowski(4, Float64, MostlyPlus)
+		@test dot(v1, v2, metric1) == 4.0 * 1.0 - (1.0 * 4.0 + 2.0 * 3.0 + 3.0 * 2.0)
+		@test dot(v1, v2, metric2) == (1.0 * 4.0 + 2.0 * 3.0 + 3.0 * 2.0) - 4.0 * 1.0
+	end
+
+	@testset "property access for 1+1D" begin
+		v = VectorLorentz(SVector(1., 2.))
+		@test v.x == 1.
+		@test v.t == 2.
+	end
+
+	@testset "spatial part extraction for 1+1D" begin
+		v = VectorLorentz(SVector(1., 2.))
+		spatial = getSpatialPart(v)
+		@test spatial.vector == SVector(1.) 
+		@test length(spatial) == 1
+		@test eltype(spatial) == Float64
+	end
+
+	@testset "temporal part extraction for 1+1D" begin
+		v = VectorLorentz(SVector(1., 2.))
+		temporal = getTemporalPart(v)
+		@test temporal == 2.
+	end
+
+	@testset "dot product for 1+1D" begin
+		v1 = VectorLorentz(SVector(1., 2.))
+		v2 = VectorLorentz(SVector(3., 4.))
+		mA = MetricMinkowski(2, Float64, MostlyPlus)
+		mB = MetricMinkowski(2, Float64, MostlyMinus)
+		@test dot(v1, v2, mA) == 1. * 3. - 2. * 4.
+		@test dot(v1, v2, mB) == -1. * 3. + 2. * 4.
+	end
+
+	@testset "dot product with metric for 1+1D" begin
+		v1 = VectorLorentz(SVector(1., 2.))
+		v2 = VectorLorentz(SVector(3., 4.))
+		metric = MetricMinkowski(2, Float64, MostlyPlus)
+		@test dot(v1, v2, metric) == 1. * 3. -  2. * 4.
+	end
+
+end
