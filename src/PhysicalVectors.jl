eea4f7ec8e32b88e0f432b26601176c54d310fb8
Rafael <rafaelab@users.noreply.github.com>
2025-04-26 20:33:11 +0200

    remove tests

--- src/PhysicalVectors.jl
+++ src/PhysicalVectors.jl
@@ -29,19 +29,3 @@ include("vector.jl")
 
 
 end
-
-
-using .PhysicalVectors
-import .PhysicalVectors: FourPosition, FourMomentum
-
-# Create a FourPosition
-pos = FourPosition([1.0, 2.0, 3.0, 4.0])
-println(pos.vector)  # Output: VectorLorentz(SVector(1.0, 2.0, 3.0, 4.0))
-
-# Create a FourMomentum
-mom = FourMomentum([1.0, 2.0, 3.0, 4.0])
-println(mom.vector)  # Output: VectorLorentz(MVector(1.0, 2.0, 3.0, 4.0))
-
-# Access properties
-println(pos.t)  # Output: 4.0 (temporal component)
-println(mom.x)  # Output: 1.0 (spatial component)
