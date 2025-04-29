2d67dbf399b71cf1529f0f7234e4c0797c90b068
Rafael <rafaelab@users.noreply.github.com>
2025-04-29 12:54:15 +0200

    merge fourQuantities.jl with lorentz.jl

--- src/vector.jl
+++ src/vector.jl
@@ -98,29 +98,6 @@ end
 include("vector/euclid.jl")
 include("vector/spatial.jl")
 include("vector/lorentz.jl")
-include("vector/fourQuantities.jl")
-
-
-# ----------------------------------------------------------------------------------------------- #
-#
-@doc """
-	norm(v::AbstractPhysicalVector{D, T}) -> T
-	norm(v::AbstractPhysicalVector{D, T}, m::AbstractMetric{D, U}) -> T
-
-Compute the norm of a Lorentz vector `v` using the default metric. 
-The norm is defined as the square root of the dot product of the vector with itself.
-
-# Input
-- `v::VectorLorentz`: the Lorentz vector whose norm is to be computed.
-
-# Output
-- `T`: The computed norm of the Lorentz vector
-"""
-@inline norm(v::AbstractPhysicalVector) = sqrt(dot(v, v))
-
-@inline norm(v::AbstractPhysicalVector{D, T}, m::AbstractMetric{D, U}) where {D, T, U} = begin
-	return sqrt(dot(v, v, m))
-end
 
 
 # ----------------------------------------------------------------------------------------------- #
