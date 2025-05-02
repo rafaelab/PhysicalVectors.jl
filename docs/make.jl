a7494db1a39d8482c7b71059fcee2c6851a86e93
Rafael <rafaelab@users.noreply.github.com>
2025-05-02 16:23:12 +0200

    (locally) working version

--- docs/make.jl
+++ docs/make.jl
@@ -1,18 +1,29 @@
 using Documenter
 
+push!(LOAD_PATH, joinpath(@__DIR__, "..", "src"))
 using PhysicalVectors
 
-DocMeta.setdocmeta!(Longinus, :DocTestSetup, :(using PhysicalVectors))
 
 
-makedocs(
+DocMeta.setdocmeta!(PhysicalVectors, :DocTestSetup, :(using PhysicalVectors))
+
+# copy README.md to docs/src/index.md
+fileIn = joinpath(@__DIR__, "..", "README.md")
+fileOut = joinpath(@__DIR__, "..", "docs", "src/index.md")
+`cp $(fileIn) $(fileOut)` |> run
+
+
+makedocs(;
 	sitename = "PhysicalVectors.jl",
+    authors = "Rafael Alves Batista",
 	format = Documenter.HTML(prettyurls = get(ENV, "CI", nothing) == "true"),
 	modules = [PhysicalVectors],
-	workdir = joinpath(@__DIR__, ".."),
 	pages = [
 		"Home" => "index.md",
-	]
-)
+		"API" => "api.md"
+		],
+	workdir = joinpath(@__DIR__, ".."),
+	doctest = true,
+	)
 
 deploydocs(repo = "github.com/rafaelab/PhysicalVectors.jl.git", versions = nothing)
