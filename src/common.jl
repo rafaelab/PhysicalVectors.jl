3fbc0740e47f04c677885a5d3a5ef82bd18eb4bd
Rafael <rafaelab@users.noreply.github.com>
2025-05-02 14:07:05 +0200

    unexport

--- src/common.jl
+++ src/common.jl
@@ -1,16 +1,11 @@
 # ----------------------------------------------------------------------------------------------- #
 #
-export
-	numberOfDimensions
-
-
-# ----------------------------------------------------------------------------------------------- #
-#
 @doc """
 	numberOfDimensions(metric::AbstractMetric{D, T}) -> D
 	numberOfDimensions(vector::AbstractPhysicalVector{D, T}) -> D
 
 Retrieve the number of dimensions of an object.
+This function is *not* exported, as it is intended for internal use within packages.
 """
 function numberOfDimensions end
 
